c0690b9e16519dcf7c28ec7b199a2b55592879f0
rizhenkolv <rizhenkolv@gmail.com>
2023-05-30 18:07:33 +0200

    Summery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar'
 import About from './components/About'
-import Footer from './components/Hero'
+import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Services from './components/Services'
 import Tours from './components/Tours'
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <section className='hero' id='home'>
       <div className='hero-banner'>
-        <h1>continue exploring</h1>
+        <h1>backroads App</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
           explicabo debitis est autem dicta.
